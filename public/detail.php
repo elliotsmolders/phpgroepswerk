63afb95cebfaeb5a80e9467dcfafe2532cbd9430
elliot smolders <elliotsmolders@gmail.com>
2021-01-23 05:38:35 +0100

    added messages when adding/removing products

--- public/detail.php
+++ public/detail.php
@@ -19,11 +19,39 @@ $availability =available($data[0]['pro_availability']);
 $id=$data[0]['pro_unique_id'];
 printHead("detail");
 printNav();
+?>
+<p class="<?php echo removeCart($msgs['removedCart'])?>" id="removeCart_message"><?php print_r($msgs['removedCart']) ; ?></p>
+<p class="<?php echo addedToCart($msgs['addedToCart'])?>" id="addedToCart_message"><?php print_r($msgs['addedToCart']) ; ?></p>
+
+<?php
 printDetailTop($image1,$image2,$image3,$image4,$name,$price,$rating);
-printDetailBottom($category,$brand,$description,$availability,$rating,$id);
+printDetailBottom($category,$brand,$description,$availability,$rating,$id,$name);
 printFooter();
 
 ?>
+<script>
+window.setTimeout("hideMessage3();", 2000);
+window.setTimeout("closeMessage3();", 9000);
+window.setTimeout("hideMessage4();", 2000);
+window.setTimeout("closeMessage4();", 9000);
+function closeMessage2(){
+    document.getElementById("logout_message").style.display=" none";
+}
+function hideMessage3(){
+    document.getElementById("removeCart_message").style.backgroundColor='transparent';
+}
+
+function closeMessage3(){
+    document.getElementById("removeCart_message").style.display=" none";
+}
+function hideMessage4(){
+    document.getElementById("addedToCart_message").style.backgroundColor='transparent';
+}
+
+function closeMessage4(){
+    document.getElementById("addedToCart_message").style.display=" none";
+}
+</script>
 </body>
 
 </html>
--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@ printHead('home');
 printNav();
 ?>
 <!-- printing out the $msgs -->
+<p class="<?php echo removeCart($msgs['removedCart'])?>" id="removedCart_message"><?php print_r($msgs['removedCart']) ; ?></p>
+<p class="<?php echo addedToCart($msgs['addedToCart'])?>" id="addedToCart_message"><?php print_r($msgs['addedToCart']) ; ?></p>
 <p class="<?php echo isRegistered($msgs['registered'])?>" id="register_message"><?php print_r($msgs['registered']) ; ?></p>
 <p class="<?php echo isLoggedOut($msgs['logged_out'])?>" id="logout_message"><?php print_r($msgs['logged_out']) ; ?></p>
 
@@ -34,6 +36,10 @@ printFooter();
     window.setTimeout("closeMessage1();", 10000);
     window.setTimeout("hideMessage2();", 3000);
     window.setTimeout("closeMessage2();", 10000);
+    window.setTimeout("hideMessage3();", 3000);
+    window.setTimeout("closeMessage3();", 10000);
+    window.setTimeout("hideMessage4();", 3000);
+    window.setTimeout("closeMessage4();", 10000);
     function hideMessage1(){
         document.getElementById("register_message").style.backgroundColor='transparent';
     }
@@ -47,6 +53,20 @@ printFooter();
 
     function closeMessage2(){
         document.getElementById("logout_message").style.display=" none";
+    }
+    function hideMessage3(){
+        document.getElementById("removedCart_message").style.backgroundColor='transparent';
+    }
+
+    function closeMessage3(){
+        document.getElementById("removedCart_message").style.display=" none";
+    }
+    function hideMessage4(){
+        document.getElementById("addedToCart_message").style.backgroundColor='transparent';
+    }
+
+    function closeMessage4(){
+        document.getElementById("addedToCart_message").style.display=" none";
     }
 </script>
 </body>
